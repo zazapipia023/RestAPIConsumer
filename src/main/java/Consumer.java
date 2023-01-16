8103cebc2549b42cee7f1180941c783493c2898d
Zaza Pipia <zaza.pipia.023@gmail.com>
2023-01-16 03:43:36 +0300

    push

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,23 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Consumer {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, String> jsonToSend = new HashMap<>();
+        jsonToSend.put("name", "test name");
+        jsonToSend.put("job", "test job");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSend);
+
+        String url = "https://reqres.in/api/users/";
+//        String response = restTemplate.getForObject(url, String.class);
+        String response = restTemplate.postForObject(url, request, String.class);
+
+        System.out.println(response);
+    }
+}
